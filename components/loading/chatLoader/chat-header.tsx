ae19bb81bbf3eca90d187e1a2a97b40a39aea58c
Priyanshu Negi <npriyanshu63@gmail.com>
2024-02-17 12:39:35 +0530

    redeploy

--- components/loading/chatLoader/chat-header.tsx
+++ components/loading/chatLoader/chat-header.tsx
@@ -6,7 +6,7 @@ export const ChatHeaderLoader = () => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />  
-        <Skeleton className="h-8 w-8 md:h-8 md:w-8 mr-2"
+        <Skeleton className="h-8 w-8 md:h-8 md:w-8 mr-3"
         />
     
       <Skeleton className=" h-4 w-6 md:h-2 md:w-4 dark:text-white" />
