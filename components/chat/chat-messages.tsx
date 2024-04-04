6b8bcecc301c68d191b7a14be0c218583a184a4c
Priyanshu Negi <npriyanshu63@gmail.com>
2024-04-05 01:09:41 +0530

    bug fix

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,7 +2,7 @@
 
 import { Fragment, useRef, ElementRef } from "react";
 import { format } from "date-fns";
-import { Member, Message, Priority, Profile } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
